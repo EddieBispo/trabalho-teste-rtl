39055022e108481819ae7d8daf885184106e2906
Pmmvito <Pmmvito@hotmail.com>
2025-11-12 18:11:16 -0300

    Adicionando os dois primeiros testes

--- src/components/imc.test.tsx
+++ src/components/imc.test.tsx
@@ -7,9 +7,25 @@ import { describe, it, expect } from 'vitest';
 import IMC from './imc';
 
 describe('IMC component', () => {
-  it('renders inputs and button', () => {});
+  it('renders inputs and button', () => {
+    render(<IMC />);
+    // Acessando o primeiro elemento [0] do array retornado:
+    expect(screen.getByPlaceholderText('Ex: 70.5')).toBeInTheDocument(); 
+    expect(screen.getByPlaceholderText('Ex: 1.75')).toBeInTheDocument(); // Assumindo que este é o primeiro (e único)
+    expect(screen.getByRole('button', { name: 'Calcular IMC' })).toBeInTheDocument();
+  });
 
-  it('shows "Abaixo do peso" for IMC < 18.5 and formats to two decimals', async () => {});
+
+
+
+  it('shows "Abaixo do peso" for IMC < 18.5 and formats to two decimals', async () => {
+    const user = userEvent.setup();
+
+    
+
+    expect(screen.getByText('Seu IMC é 4.99 - Abaixo do peso'));
+
+  });
 
   it('classifies Peso normal, Sobrepeso and Obesidade categories correctly', async () => {});
 
@@ -18,4 +34,5 @@ describe('IMC component', () => {
   it('shows error when peso is non-positive', async () => {});
 
   it('shows error when altura is non-positive', async () => {});
+  
 });
